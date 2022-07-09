266b34d17d0cebe4d58ea39855ed04694885880a
Ali Sadat <alisadat@Alis-MacBook-Air.local>
2022-07-09 20:16:44 +1000

    comms between esp32 and smartphone works

--- sketch_jun25a/sketch_jun25a.ino
+++ sketch_jun25a/sketch_jun25a.ino
@@ -1,39 +1,109 @@
-// https://github.com/innoveit/react-native-ble-manager
-// https://github.com/innoveit/react-native-ble-manager
-// https://randomnerdtutorials.com/esp32-bluetooth-classic-arduino-ide/
-// https://randomnerdtutorials.com/esp32-bluetooth-classic-arduino-ide/
+// https://github.com/palmmaximilian/ReactNativeArduinoBLE
+// https://github.com/palmmaximilian/ReactNativeArduinoBLE
 
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
 
-#include "BluetoothSerial.h"
+// BLE SECTION
+BLEServer *pServer = NULL;
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+BLECharacteristic *message_characteristic = NULL;
+BLECharacteristic *box_characteristic = NULL;
 
-BluetoothSerial SerialBT;
+String boxValue = "0";
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 
-String text = "";
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 
-void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test222"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-}
+#define MESSAGE_CHARACTERISTIC_UUID "6d68efe5-04b6-4a85-abc4-c2670b7bf7fd"
+#define BOX_CHARACTERISTIC_UUID "f27b53ad-c63d-49a0-8c0f-9f297e6cc520"
 
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    Serial.println("Connected");
+  };
+
+  void onDisconnect(BLEServer *pServer)
+  {
+    Serial.println("Disconnected");
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
- if (SerialBT.available() > 0) {
-    text = SerialBT.readStringUntil('\n');
-    text.remove(text.length()-1, 1);
-    Serial.println(text);
-    if(text == "go") {
-      Serial.println("Info");
+};
+
+class CharacteristicsCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    Serial.print("Value Written ");
+    Serial.println(pCharacteristic->getValue().c_str());
+
+    if (pCharacteristic == box_characteristic)
+    {
+      boxValue = pCharacteristic->getValue().c_str();
+      box_characteristic->setValue(const_cast<char *>(boxValue.c_str()));
+      box_characteristic->notify();
     }
   }
-  delay(20);
+};
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Test___111111");
+  // Create the BLE Device
+  BLEDevice::init("BLEExample");
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  delay(100);
+
+  // Create a BLE Characteristic
+  message_characteristic = pService->createCharacteristic(
+      MESSAGE_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
+
+  box_characteristic = pService->createCharacteristic(
+      BOX_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
+
+  // Start the BLE service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+
+  message_characteristic->setValue("Message one");
+  message_characteristic->setCallbacks(new CharacteristicsCallbacks());
+
+  box_characteristic->setValue("0");
+  box_characteristic->setCallbacks(new CharacteristicsCallbacks());
+
+  Serial.println("Waiting for a client connection to notify...");
+}
+
+void loop()
+{
+  message_characteristic->setValue("Message one");
+  message_characteristic->notify();
+
+  Serial.println("Step 111...");
+  delay(1000);
+
+  message_characteristic->setValue("Message Two");
+  message_characteristic->notify();
+  
+  Serial.println("Step 222...");
+  delay(1000);
 }
